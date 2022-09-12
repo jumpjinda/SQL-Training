aa7ff4a841102ff63936bbed03c2cf60a0930afc
Tanawat Jinda <jumpjinda@gmail.com>
2022-09-13 02:11:00 +0700

    Update and rename Query SQL to 3. Query SQL.sql

--- 3. Query SQL.sql
+++ 3. Query SQL.sql
@@ -1,7 +1,7 @@
 -- 1. What is the total amount each customer spent at the restaurant?
 
 SELECT
-  	s.customer_id,
+    s.customer_id,
     SUM(m.price) AS total_spent
 FROM dannys_diner.sales s
 INNER JOIN dannys_diner.menu m
